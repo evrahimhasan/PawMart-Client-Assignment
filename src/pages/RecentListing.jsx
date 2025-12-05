28536be56550e910042e3de4d48889af7cb8befb
evrahimhasan <evrahimhasanemran.com>
2025-12-05 12:53:34 +0600

    recent listing card design modify

--- src/pages/RecentListing.jsx
+++ src/pages/RecentListing.jsx
@@ -17,16 +17,27 @@ const RecentListing = () => {
                 {listings.slice(0, 6).map((listing) => (
                     <div
                         key={listing._id}
-                        className="rounded-2xl p-4 shadow hover:shadow-lg transition bg-white"
+                        className='rounded-2xl p-4 shadow hover:shadow-lg transition bg-white duration-300 ease-out 
+                        hover:translate-y-3 hover:scale-[1.03]'
                     >
-                        <img
-                            src={listing.image}
-                            alt={listing.name}
-                            className="w-full h-[450px] object-cover rounded-lg"
-                        />
-                        <h3 className="text-xl font-semibold mt-3">{listing.name}</h3>
-                        <p className="text-green-600 font-medium">${listing.price}</p>
-                        <p className="text-yellow-500">Date: {listing.date}</p>
+                        <div className="mb-5">
+                            <img
+                                className="w-full h-[450px] object-cover rounded-lg"
+                                src={listing.image}
+                                alt=""
+                            />
+                        </div>
+                        <h2 className="text-2xl font-bold">{listing.name}</h2>
+                        <div className="flex justify-between font-semibold mt-3 text-xl">
+                            <h1>Owner Name: {listing.date}</h1>
+                            <p>Price:{listing.price}$</p>
+                        </div>
+                        {/* <p className="text-green-600 font-medium">${listing.price}</p>
+                        <p className="text-yellow-500">Date: {listing.date}</p> */}
+                        <div className="flex gap-2 items-center ">
+                            <span className="text-green-500"></span>
+                            <h1 className="font-semibold mt-2 text"> {listing.location}</h1>
+                        </div>
                         <Link
                             to={`/pets-details/${listing?._id}`}
                             className="mt-3 inline-block bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700"
